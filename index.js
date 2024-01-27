b7e27f04ab2db6023a6eea22b274127d3bb815bf
Sagar051989 <156504640+Sagar051989@users.noreply.github.com>
2024-01-27 15:54:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+// ASSIGNMENT1
+
+// a = 2;
+// b = 5;
+// console.log(2+5);
+// console.log(a+b);
+
+// ASSIGNMENT2
+
+// a = 5;
+// b = 3;
+// if(a<10 && a>b){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// ASSIGNMENT3
+
+// a = 10;
+// b = 20;
+// if(a/10 && b/10){
+//     console.log(true);
+// } 
+// else{
+//     console.log(false);
+// }
+
+
+// a = 12;
+// b = 21;
+// if(a%10 == 0 && b%10 ==0){
+//     console.log(true);
+// } 
+// else{
+//     console.log(false);
+// }
+
+
+// a = 10;
+// b = 21;
+// if(a%10 == 0 || b%10 == 0 ){
+//     console.log(true);
+// } 
+// else{
+//     console.log(false);
+// }
+
+
+// ASSIGNMENT4
+// a = 4567
+// console.log(Math. floor(a/1000));
+
+
+// ASSIGNMENT6
+
+// a = 2;
+// b = 9;
+// console.log(9%2);
+
+
+// ASSIGNMENT7
+
+// a = 2;
+// b = 5;
+// console.log(a*b);
+
+
+
+// ASSIGNMENT8
+// a = 50;
+// b = 20;
+// c = 100;
+// console.log(a+b+c);
+
+// a = 50;
+// b = 20;
+// c = 100;
+// d = a+b+c;
+// console.log(d/3);
+
+
+
+// SECOND SHEET ASSIGNMENT
+
+// ASSIGNMENT2
+
+let getvalue;
+getvalue = (prompt("Enter the value"));
+switch(true){
+    case getvalue : "P" || "P";
+    char = "PrepBytes";
+    break; 
+
+    case getvalue : "Z" || "z";
+    char = "zenith";
+    break; 
+
+    case getvalue : "E" || "e";
+    char = "Expert Coder";
+    break; 
+
+    case getvalue : "P" || "p";
+    char = "Data Structure";
+    break; 
+    default:
+    "Invalid Marks Obtained";
+
+}
+alert(char);
+
+
